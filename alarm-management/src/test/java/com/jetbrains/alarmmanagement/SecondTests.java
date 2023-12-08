7416f570b5a0805fd0d0f192300a2917f996498b
Ivan Vasilev <vanokvas@gmail.com>
2023-12-08 19:29:23 +0400

    Increase delay for second test batch

--- alarm-management/src/test/java/com/jetbrains/alarmmanagement/SecondTests.java
+++ alarm-management/src/test/java/com/jetbrains/alarmmanagement/SecondTests.java
@@ -11,31 +11,31 @@ class SecondTests {
 
 	@Test
 	void secondContextLoads() throws InterruptedException {
-		Thread.sleep(10000);
+		Thread.sleep(20000);
 		assertEquals(1,1);
 	}
 
 	@Test
 	void secondContextLoads_1() throws InterruptedException {
-		Thread.sleep(10000);
+		Thread.sleep(20000);
 		assertEquals(2,2);
 	}
 
 	@Test
 	void secondContextLoads_2() throws InterruptedException {
-		Thread.sleep(10000);
+		Thread.sleep(20000);
 		assertEquals(2,2);
 	}
 
 	@Test
 	void secondContextLoads_3() throws InterruptedException {
-		Thread.sleep(10000);
+		Thread.sleep(20000);
 		assertEquals(2,2);
 	}
 
 	@Test
 	void secondContextLoads_4() throws InterruptedException {
-		Thread.sleep(10000);
+		Thread.sleep(20000);
 		assertEquals(2,2);
 	}
 }
